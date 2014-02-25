6d90b6a24468e72484b76d6c2f2016299c6273d3
Ben Wu <wubin28@gmail.com>
2014-02-25 22:14:33 +0800

    Implemented method CityClock.setLocalTime().

--- src/main/java/tbc/waterfall/CityClock.java
+++ src/main/java/tbc/waterfall/CityClock.java
@@ -3,5 +3,9 @@ package tbc.waterfall;
 /**
  * Created by ben on 14-2-25.
  */
-public class CityClock {
+public class CityClock extends Clock {
+    @Override
+    public void setLocalTime(int localTime) {
+        super.localTime = localTime;
+    }
 }
