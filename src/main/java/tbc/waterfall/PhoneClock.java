207ef5b8b4d6d3fa98a05c326c78876f6e889342
Ben Wu <wubin28@gmail.com>
2014-02-25 22:11:26 +0800

    Added field PhoneClock.utcTime.

--- src/main/java/tbc/waterfall/PhoneClock.java
+++ src/main/java/tbc/waterfall/PhoneClock.java
@@ -4,6 +4,8 @@ package tbc.waterfall;
  * Created by ben on 14-2-25.
  */
 public class PhoneClock extends Clock {
+    private UtcTime utcTime;
+
     @Override
     public void setLocalTime(int localTime) {
         super.localTime = localTime;
