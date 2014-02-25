df70c93ea4a8975f7c270819cfdd2878b7c71db0
Ben Wu <wubin28@gmail.com>
2014-02-25 22:03:37 +0800

    Implemented method PhoneClock.setLocalTime() according to the class diagram.

--- src/main/java/tbc/waterfall/PhoneClock.java
+++ src/main/java/tbc/waterfall/PhoneClock.java
@@ -3,5 +3,10 @@ package tbc.waterfall;
 /**
  * Created by ben on 14-2-25.
  */
-public class PhoneClock {
+public class PhoneClock extends Clock {
+    @Override
+    public void setLocalTime(int localTime) {
+        this.localTime = localTime;
+        this.utcTime.setUtcZeroTime(localTime - UTC_OFFSET);
+    }
 }
