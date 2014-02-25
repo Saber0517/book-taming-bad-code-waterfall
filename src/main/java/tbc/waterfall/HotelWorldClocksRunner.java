7d721fa5dbe9701258526a96d5722299b3272a93
Ben Wu <wubin28@gmail.com>
2014-02-25 23:03:33 +0800

    Added the main() method to class HotelWorldClocksRunner and wrote the expected code there.

--- src/main/java/tbc/waterfall/HotelWorldClocksRunner.java
+++ src/main/java/tbc/waterfall/HotelWorldClocksRunner.java
@@ -4,4 +4,17 @@ package tbc.waterfall;
  * Created by ben on 14-2-25.
  */
 public class HotelWorldClocksRunner {
+    public static void main(String[] args) {
+        TimeSubject utcTime = new UtcTime();
+        utcTime.attach("beijing", new CityClock(8));
+        utcTime.attach("london", new CityClock(0));
+        utcTime.attach("moscow", new CityClock(4));
+        utcTime.attach("sydney", new CityClock(10));
+        utcTime.attach("newYork", new CityClock(-5));
+        Clock phoneClock = new PhoneClock(utcTime);
+
+        phoneClock.setLocalTime(9);
+
+        utcTime.printTimeOfAllClocks();
+    }
 }
