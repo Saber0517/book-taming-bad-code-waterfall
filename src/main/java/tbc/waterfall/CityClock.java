93acb9648e48a562d7653b83d7f3d9bdcb8f9e95
Ben Wu <wubin28@gmail.com>
2014-02-25 23:13:55 +0800

    Added constructor CityClock(int utcOffset).

--- src/main/java/tbc/waterfall/CityClock.java
+++ src/main/java/tbc/waterfall/CityClock.java
@@ -4,6 +4,10 @@ package tbc.waterfall;
  * Created by ben on 14-2-25.
  */
 public class CityClock extends Clock {
+    public CityClock(int utcOffset) {
+        super();
+    }
+
     @Override
     public void setLocalTime(int localTime) {
         super.localTime = localTime;
