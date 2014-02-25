fbaf74e1d129152e0e84e33149626e60fd8ec026
Ben Wu <wubin28@gmail.com>
2014-02-25 12:57:30 +0800

    Added an int field utcZeroTime to class UtcTime.

--- src/main/java/tbc/waterfall/UtcTime.java
+++ src/main/java/tbc/waterfall/UtcTime.java
@@ -4,6 +4,8 @@ package tbc.waterfall;
  * Created by ben on 14-2-25.
  */
 public class UtcTime extends TimeSubject {
+    private int utcZeroTime;
+
     @Override
     public void notifyAllClocks() {
         for (Clock clock : super.clocks.values()) {
