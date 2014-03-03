f1a4dfef9875b1e7916b39cb20e1395c3d64bd2b
Ben Wu <wubin28@gmail.com>
2014-03-03 14:18:38 +0800

    Created method PhoneClock.setUtcTime(UtcTime).

--- src/main/java/tbc/waterfall/PhoneClock.java
+++ src/main/java/tbc/waterfall/PhoneClock.java
@@ -15,4 +15,8 @@ public class PhoneClock extends Clock {
         super.localTime = localTime;
         this.utcTime.setUtcZeroTime(localTime - UTC_OFFSET);
     }
+
+    public void setUtcTime(UtcTime utcTime) {
+        this.utcTime = utcTime;
+    }
 }
